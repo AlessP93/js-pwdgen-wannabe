a15385e024418957f1fcd3f27d8c0dbd01a3a985
AlessP93 <alespiscino@libero.it>
2022-04-27 15:39:31 +0200

    fine esercizio

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Chiedi all’utente il suo nome
+const nomeUtente = prompt("Ciao, inserisci il tuo nome");
+
+// poi chiedi il suo cognome
+const cognomeUtente = prompt("ora inserisci il tuo cognome");
+
+// poi chiedi il suo colore preferito
+const coloreUtente = prompt("infine inserisci il tuo colore preferito");
+
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+document.getElementById("title").innerHTML = nomeUtente + cognomeUtente + coloreUtente + 21
